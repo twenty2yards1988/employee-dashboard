2fede2aca69e1618c9a484ba2f60bb93c3b84be8
twenty2yards1988 <twenty2yards1988@gmail.com>
2026-06-06 19:14:03 +0530

    Added navbar and sidebar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Employee Dashboard</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+function Sidebar() {
+  return (
+    <aside>
+      <ul>
+        <li>Dashboard</li>
+        <li>Employees</li>
+        <li>Reports</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
